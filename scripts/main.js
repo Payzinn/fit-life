c94ed7b3f6bcd0bc814341653363f327018a7583
Payzinn <110055973+Payzinn@users.noreply.github.com>
2024-05-31 16:20:10 +0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -19,23 +19,34 @@ function calculateBMI() {
   var heightInput = document.querySelector('input[placeholder="Ваш рост (см)"]');
   var weightInput = document.querySelector('input[placeholder="Ваш вес (кг)"]');
 
-  if (heightInput.value && weightInput.value) {
-    var height = parseInt(heightInput.value);
-    var weight = parseInt(weightInput.value);
-
-    var bmi = (weight / Math.pow(height / 100, 2)).toFixed(2);
-
-    alert("Ваш ИМТ: " + bmi);
-  } else {
+  if (!heightInput.value || !weightInput.value) {
     alert("Пожалуйста, заполните все поля.");
+    return;
   }
+
+  if (isNaN(heightInput.value) || isNaN(weightInput.value)) {
+    alert("Введите только числа в поля роста и веса.");
+    return;
+  }
+
+  var height = parseInt(heightInput.value);
+  var weight = parseInt(weightInput.value);
+
+  if (height < 10 || weight < 1) {
+    alert("Введены некорректные данные. Проверьте поля роста и веса.");
+    return;
+  }
+
+  var bmi = (weight / Math.pow(height / 100, 2)).toFixed(2);
+
+  alert("Ваш ИМТ: " + bmi);
 }
 
 var calculateButton = document.querySelector('.btn-primary');
 calculateButton.addEventListener('click', calculateBMI);
 
-var openModalButton = document.querySelector('.btn-openModal'); 
-var modal = new bootstrap.Modal(document.getElementById('modal1')); 
+var openModalButton = document.querySelector('.btn-openModal');
+var modal = new bootstrap.Modal(document.getElementById('modal1'));
 
 openModalButton.addEventListener("click", function() {
   modal.show();
